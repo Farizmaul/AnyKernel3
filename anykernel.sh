fef7eea73217c5563ff101238514f498f5a7fbe0
RicoAyuba <87293335+Farizmaul@users.noreply.github.com>
2024-06-10 17:05:31 +0700

    Rebrand kernel name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Wellcome to New Hyper-X kernel
+kernel.string= Wellcome to NightQueen kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
